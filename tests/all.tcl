4ee9e5b8c01d0993eddf3b0db3a90c568093e5f3
aotto1968 <aotto1968@t-online.de>
2025-04-30 10:59:14 +0200

    cleanup for branch atlsh-1-2

--- tests/all.tcl
+++ tests/all.tcl
@@ -7,8 +7,15 @@
 #:
 
 package require Tcl 8.5
-package require -exact tcltest 2.3.8
-namespace import tcltest::*
-configure {*}$argv -testdir [file dir [info script]] -constraints parallel
+package require tcltest 2.3
 
+namespace import tcltest::*
+
+# the directory where "all.tcl" is located is also used as "-testdir"
+configure {*}$argv -testdir [file dir [info script]]
+
+# Some tests simply do not work in parallel mode, disable them with "!parallel".
+configure -constraints parallel
+
+# extra feature to exit with 1 on failed test
 runAllTop
--- tests/remote.tcl
+++
@@ -1,1 +0,0 @@
-../atlsh/tests/remote.tcl
